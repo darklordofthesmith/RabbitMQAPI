bddcfd92be74c656718cc3eb59363e9e645673ea
David Smith <davesmith@gmail.com>
2018-02-10 14:50:46 -0800

    First

---
+++ RabbitMQAPI/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQAPI.Services;
+
+namespace RabbitMQAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "RabbitMQ", ".NET Core" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+            Send sender = new Send("Rabbit" + DateTime.Now.ToString());
+        }
+    }
+}
---
+++ RabbitMQAPI/Services/Send.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMQAPI.Services
+{
+    public class Send
+    {
+        public Send(string message)
+        {
+            //open connection
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "SimpleMessage",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange:"",
+                                     routingKey: "SimpleMessage",
+                                     basicProperties: null,
+                                     body: body);                      
+            }
+        }
+    }
+}
